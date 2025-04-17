4e3dd7982cd6be503f52f328ae029880523b5d63
Joeyzeeee <krisna.adstr@gmail.com>
2025-04-17 18:38:56 +0700

    Class Penilaian

---
+++ Penilaian.java
@@ -0,0 +1,26 @@
+public class Penilaian {
+    Mahasiswa mahasiswa;
+    MataKuliah mataKuliah;
+    double nilaiTugas;
+    double nilaiUTS;
+    double nilaiUAS;
+    double nilaiAkhir;
+
+    public Penilaian(Mahasiswa mahasiswa, MataKuliah mataKuliah, double tugas, double uts, double uas) {
+        this.mahasiswa = mahasiswa;
+        this.mataKuliah = mataKuliah;
+        this.nilaiTugas = tugas;
+        this.nilaiUTS = uts;
+        this.nilaiUAS = uas;
+        hitungNilaiAkhir();
+    }
+
+    public void hitungNilaiAkhir() {
+        nilaiAkhir = (nilaiTugas * 0.3) + (nilaiUTS * 0.3) + (nilaiUAS * 0.4);
+    }
+
+    public void tampilPenilaian() {
+        System.out.println(mahasiswa.nama + " | " + mataKuliah.namaMK + " | Nilai Akhir: " + nilaiAkhir);
+    }
+
+}
