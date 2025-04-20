8ff474c6b3a0db0404ada957fdeedfa9cdcf6468
tsoareu <karahmaa25@gmail.com>
2025-04-20 21:16:24 +0700

    MainUTS(Menu dan sc pilihan)

--- MainUTS.java
+++ MainUTS.java
@@ -23,5 +23,18 @@ public class MainUTS {
                 new Penilaian(mhs[2], mk[1], 85, 90, 95),
                 new Penilaian(mhs[2], mk[2], 80, 90, 65)
         };
+
+        int pilihan;
+        do {
+            System.out.println("=== MENU SISTEM AKADEMIK ===");
+            System.out.println("1. Tampilkan Daftar Mahasiswa");
+            System.out.println("2. Tampilkan Daftar Mata Kuliah");
+            System.out.println("3. Tampilkan Data Penilaian");
+            System.out.println("4. Urutkan Mahasiswa Berdasarkan Nilai Akhir");
+            System.out.println("5. Cari Mahasiswa Berdasarkan NIM");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu: ");
+            pilihan = sc.nextInt();
+            sc.nextLine();
 }
 } 
